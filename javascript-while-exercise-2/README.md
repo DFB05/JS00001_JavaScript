e497f270dbcbb1d219b4c6b488f1e47818a43973
Roodney Felix Borges <102576004+DFB05@users.noreply.github.com>
2025-02-23 19:57:07 -0300

    Created javascript-while-exercise-2 folder and created README.md and script.js

---
+++ javascript-while-exercise-2/README.md
@@ -0,0 +1,3 @@
+## 2. Somar os números de 1 a 100
+
+> Use um while para somar todos os números de 1 a 100 e exiba o resultado.
---
+++ javascript-while-exercise-2/script.js
@@ -0,0 +1,9 @@
+let contador = 1;
+let somatorio = 0;
+
+while (contador <= 100) {
+  somatorio += contador; // somatorio = somatorio + contador
+  contador++; // contador = contador + 1
+}
+
+console.log(somatorio); // Exibe 5050
