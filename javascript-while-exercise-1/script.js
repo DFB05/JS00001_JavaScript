116c955f34d9a175755c69ea67f46f7b70b91fde
Roodney Felix Borges <102576004+DFB05@users.noreply.github.com>
2025-02-23 18:58:43 -0300

    create javascript-while-exercise-1
    Folder created, with script.js file inside.

---
+++ javascript-while-exercise-1/script.js
@@ -0,0 +1,12 @@
+/*
+1. Imprimir números de 1 a 10
+
+Crie um loop que imprima os números de 1 a 10 no console.
+*/
+
+let contador = 1
+
+while (contador <= 10) {
+  console.log (contador);
+  contador++
+}
